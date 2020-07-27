8587c5d1a9705bfc036a29474cb73267ea574067
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2020-07-28 00:10:23 +0530

    built in pipes"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pipedemo';
+export class AppComponent implements OnInit {
+  title = ''
+  
+servers= [{
+  'serverName' : 'Andrew Server',
+  'serverType' : 'Testing',
+  'serverStatus' : 'offline',
+  'serverCreated' : new Date ("12, 02, 2020"),
+  'cost': 40
+},
+{
+  'serverName' : 'Thomson Server',
+  'serverType' : 'Staging',
+  'serverStatus' : 'online',
+  'serverCreated' : new Date ("12, 02, 2020"),
+  'cost': 40
+},
+{
+  'serverName' : 'Thor Server',
+  'serverType' : 'Devlopment',
+  'serverStatus' : 'offline',
+  'serverCreated' : new Date ("12, 02, 2020"),
+  'cost': 40
+},
+{
+  'serverName' : 'Lily Server',
+  'serverType' : 'Production',
+  'serverStatus' : 'offline',
+  'serverCreated' : new Date ("12, 02, 2020"),
+  'cost': 40
+}]
+
+ngOnInit(){
+  
+}
+
+addServer(){
+ let server = {
+  'serverName' : 'new Server',
+  'serverType' : 'Production',
+  'serverStatus' : 'offline',
+  'serverCreated' : new Date ("12, 02, 2020"),
+  'cost': 40
+ } 
+ this.servers.push(server);
+}
+
 }
